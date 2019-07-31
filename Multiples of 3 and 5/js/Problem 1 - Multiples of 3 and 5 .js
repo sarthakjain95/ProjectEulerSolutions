3d902025dae11d178c4e372bda71dcd9f148d837
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-07-31 18:28:13 +0530

    Update Problem 1 - Multiples of 3 and 5 .js

--- Multiples of 3 and 5/js/Problem 1 - Multiples of 3 and 5 .js
+++ Multiples of 3 and 5/js/Problem 1 - Multiples of 3 and 5 .js
@@ -7,9 +7,9 @@
 // looking at the solution
 
 
-function GetSumOfMultiples(limit){
+function GetSumOfMultiples(limit=1000){
 	let sum = 0;
-	for(let i=0;i<1000;i++){
+	for(let i=0;i<limit;i++){
 		if( i%3==0 || i%5==0 ) sum+=i;
 	}
 	return sum;
