268b3246af7a799b42a78dcfa5b46c2eb2499517
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-07-31 18:46:49 +0530

    Add files via upload

---
+++ Even Fibonacci numbers/js/Even Fibonacci Numbers.js
@@ -0,0 +1,25 @@
+
+// https://github.com/sarthakjain95/ProjectEulerSolutions
+// Problem 2
+// Even Fibonacci numbers
+
+// I wish you try attempting the problem yourself before
+// looking at the solution.
+// ...
+
+function GetEvenFibboSum(limit){ 
+	let num1=0, num2=1, sum=0;
+	for(let fibbo=0;;){
+		fibbo = num1 + num2;
+		num1 = num2;
+		num2 = fibbo;
+		if( fibbo%2==0 ) sum+=fibbo;	
+		if(fibbo>limit) break;
+	}
+	return sum;
+ }
+
+console.log("Sum is", GetEvenFibboSum(4000000) );
+
+// ProjectEulerSolutions
+// Sarthak Jain
