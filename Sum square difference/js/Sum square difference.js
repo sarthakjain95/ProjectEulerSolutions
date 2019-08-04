659197a659c7c73ecb6ef4d0069fc20546e2b029
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-04 17:52:07 +0530

    Add files via upload

---
+++ Sum square difference/js/Sum square difference.js
@@ -0,0 +1,18 @@
+// https://github.com/sarthakjain95/ProjectEulerSolutions
+// Problem 6
+// Sum square difference
+
+// I wish you try attempting the problem yourself before
+// looking at the solution.
+// ...
+
+function GetSomeDifference(lim){
+	let res=0, temp=0;
+	for(let i=0;i<=lim;i++) res+= i**2;
+	for(let x=0;x<=lim;x++) temp+=x;
+	res = temp ** 2 - res;
+	return res;
+}
+
+// ProjectEulerSolutions
+// Sarthak Jain
