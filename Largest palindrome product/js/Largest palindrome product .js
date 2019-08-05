02d08ec199536f18a9daf7660298be5e8d246be1
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-05 20:07:48 +0530

    Update Largest palindrome product .js

--- Largest palindrome product/js/Largest palindrome product .js
+++ Largest palindrome product/js/Largest palindrome product .js
@@ -3,7 +3,7 @@
 // Problem 4
 // Largest palindrome product
 
-// I wish you try attempting the problem yourself before
+// Please try attempting the problem yourself before
 // looking at the solution.
 // ...
 
