d232de105bb2b4344ccb3348b15ab9fd5e30370f
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-05 20:07:28 +0530

    Update Even Fibonacci Numbers.js

--- Even Fibonacci numbers/js/Even Fibonacci Numbers.js
+++ Even Fibonacci numbers/js/Even Fibonacci Numbers.js
@@ -3,7 +3,7 @@
 // Problem 2
 // Even Fibonacci numbers
 
-// I wish you try attempting the problem yourself before
+// Please try attempting the problem yourself before
 // looking at the solution.
 // ...
 
