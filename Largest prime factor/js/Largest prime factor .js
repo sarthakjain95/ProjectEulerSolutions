8af6202aa2d46aece96c2bd8e977241f35223afa
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-05 20:08:04 +0530

    Update Largest prime factor .js

--- Largest prime factor/js/Largest prime factor .js
+++ Largest prime factor/js/Largest prime factor .js
@@ -3,7 +3,7 @@
 // Problem 3
 // Largest Prime Factor
 
-// I wish you try attempting the problem yourself before
+// Please try attempting the problem yourself before
 // looking at the solution.
 // ...
 
