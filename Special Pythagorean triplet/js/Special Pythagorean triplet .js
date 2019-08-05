713f38a2c279f28786bdc74b34684b787c520365
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-05 20:09:14 +0530

    Update Special Pythagorean triplet .js

--- Special Pythagorean triplet/js/Special Pythagorean triplet .js
+++ Special Pythagorean triplet/js/Special Pythagorean triplet .js
@@ -3,7 +3,7 @@
 // Problem 9
 // Special Pythagorean triplet
 
-// I wish you try attempting the problem yourself before
+// Please try attempting the problem yourself before
 // looking at the solution.
 // ...
 
