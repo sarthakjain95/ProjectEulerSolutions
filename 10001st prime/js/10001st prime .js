a66e38cfd009614e35065e1b4aaf8eb367457fb3
Sarthak Jain <53367703+sarthakjain95@users.noreply.github.com>
2019-08-05 20:06:55 +0530

    Update 10001st prime .js

--- 10001st prime/js/10001st prime .js
+++ 10001st prime/js/10001st prime .js
@@ -3,7 +3,7 @@
 // Problem 7
 // 10001st prime
 
-// I wish you try attempting the problem yourself before
+// Please try attempting the problem yourself before
 // looking at the solution.
 // ...
 
