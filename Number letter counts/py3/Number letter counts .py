3dece1f784634638dbc5dcca62b4433ed36f2b2d
sarthakjain95 <saarthakjain95@gmail.com>
2019-08-22 15:55:42 +0530

    Adding Number letter counts problem

---
+++ Number letter counts/py3/Number letter counts .py
@@ -0,0 +1,14 @@
+
+# https://github.com/sarthakjain95/ProjectEulerSolutions
+# Problem 17
+# Number letter counts
+
+# Please try attempting the problem yourself before
+# looking at the solution.
+# ...
+
+def countNumberLetters(n):
+	return sum([int(x) for x in list(str(n))])
+
+# ProjectEulerSolutions
+# Sarthak Jain
