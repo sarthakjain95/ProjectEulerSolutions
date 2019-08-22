c241d320d0cd9de8f1da6f9c7e971a732b2a16bd
sarthakjain95 <saarthakjain95@gmail.com>
2019-08-22 16:37:02 +0530

    Re:Adding Power digit sum

--- Power digit sum/js/Power digit sum .js
+++ Power digit sum/js/Power digit sum .js
@@ -1,14 +1,14 @@
 
 // https://github.com/sarthakjain95/ProjectEulerSolutions
-// Problem 17
-// Number letter counts
+// Problem 16
+// Power digit sum
 
 // Please try attempting the problem yourself before
 // looking at the solution.
 // ...
 
 const add = (a,b)=>Number(a)+Number(b);
-const countNumberLetters = (n)=>n.toString().split('').reduce(add,0);
+const powerDigitsSum = (n)=>n.toString().split('').reduce(add,0);
 // Use String(BigInt(Calculation))
 
 // ProjectEulerSolutions
--- Power digit sum/py3/Power digit sum .py
+++ Power digit sum/py3/Power digit sum .py
@@ -1,13 +1,13 @@
 
 # https://github.com/sarthakjain95/ProjectEulerSolutions
-# Problem 17
-# Number letter counts
+# Problem 16
+# Power digit sum
 
 # Please try attempting the problem yourself before
 # looking at the solution.
 # ...
 
-def countNumberLetters(n):
+def powerDigitsSum(n):
 	return sum([int(x) for x in list(str(n))])
 
 # ProjectEulerSolutions
